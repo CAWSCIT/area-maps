643b0f7edfac4d800f8bf9689ea50da9b8ffb773
Kalob Taulien <kalob.taulien@gmail.com>
2025-09-03 16:56:21 -0600

    Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import './App.css'
 import 'leaflet/dist/leaflet.css';
 import Areas from './areas/Areas.json';
 
 import GeoMapComponent from './GeoMapComponent.jsx';
+
 
 function App() {
   return (
